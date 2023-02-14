2b6abb865c434a747716680bb3d2526da24827cb
“MariamOmolaragit config --global user.name “MariamOmolara <omolaramariam111@gmail.comgit config --global user.email omolaramariam111@gmail.com>
2023-02-14 13:35:34 +0100

    index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ console.log (multiply (10,15));
 let FirstName = "Mariam Omolara";
 let LastName = "Abdulkareem";
 let StuternTrack = "Frontend developer";
-let skills = "Frontend Development and Product Design";
+let Skills = "Frontend Development and Product Design";
 let NigeriaFlag = "Green";
 let YearOfNigeriansIndependence = 1960;
 
-let Sentence = `My name is ${FirstName} ${LastName}. I'm learning to be a ${StuternTrack} and I have some other skills which are ${skills}. My country is Nigeria, with ${favColor} and white color of flag, got her independence in the year ${yearOfNigeriansIndependence}.`;
+let Sentence = `My name is ${FirstName} ${LastName}. I'm learning to be a ${StuternTrack} and I have some other skills which are ${Skills}. My country is Nigeria, with ${NigeriaFlag} and white color of flag, got her independence in the year ${YearOfNigeriansIndependence}.`;
 
 console.log(Sentence);
